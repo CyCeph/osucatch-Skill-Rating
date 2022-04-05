c3d44695e0b2be13669c4bbd93b7a36e2134d8f4
K3VRAL <kevinbestkiller@gmail.com>
2022-04-05 21:29:55 +0200

    Automated titles -> relys on the spreadsheet to give info

--- lists/reworked.js
+++ lists/reworked.js
@@ -41,9 +41,9 @@ request.onreadystatechange = async () => {
               let aSplit = a[localStorage.getItem("sort")].split(":");
               return parseFloat(bSplit[0] * 60 * 24 + bSplit[1] * 60 + bSplit[2]) - parseFloat(aSplit[0] * 60 * 24 + aSplit[1] * 60 + aSplit[2]);
             case "Title":
-              let order = ["Creator", "Grandmaster", "GM Candidate", "Honorary GM", "Dan", "Master", "Expert", "Advanced", "Amateur", "Beginner", "Untitled"];
-              let indexOfA = a[localStorage.getItem("sort")].includes("Dan") ? order.indexOf("Dan") : order.indexOf(a[localStorage.getItem("sort")]);
-              let indexOfB = b[localStorage.getItem("sort")].includes("Dan") ? order.indexOf("Dan") : order.indexOf(b[localStorage.getItem("sort")]);
+              //TODO from K3: Include all necessary Titles (see note in `order of titles` note)
+              let indexOfA = order.indexOf(a[localStorage.getItem("sort")]);
+              let indexOfB = order.indexOf(b[localStorage.getItem("sort")]);
               //TODO: Figure out Dan order. Currently set after Honorary GM and is not sorted by in correct order.
               if (indexOfA < indexOfB) {
                 return -1;
@@ -107,7 +107,29 @@ let onChangeView = (args) => {
     }
     request.open("GET", "https://docs.google.com/spreadsheets/d/1s-ItBZwDzWb_taDPD2L2jrUbNzl4pxjSgXcE5dza4tc/export?format=csv&id=1s-ItBZwDzWb_taDPD2L2jrUbNzl4pxjSgXcE5dza4tc&gid=1043073592", true);
     request.send();
+
+// order of titles
+/*Note from K3: Would also recommend that the spreadsheet information based on the Title is consistent: there are no information on "Honorary GM" and "Untitled" (plus
+                "GM Candidate" is "Grandmaster Candidate" which can make this a bit annoying and is recommended to choose one over the other
+                for better automation) which if it should be title, it should be mentioned. The reason for this is that we can let the computer do all the processing for the order without
+                having to update it each time for both the source code of the spreadsheets and javascript. Only the spreadsheets will need to be updated and the javascript
+                will just refer to the spreadsheets based on the information.*/
+let getOrder = new XMLHttpRequest();
+getOrder.onreadystatechange = async () => {
+  if (getOrder.readyState == 4 && getOrder.status == 200) {
+    let orderInformation = await csv().fromString(getOrder.responseText);
+    let found = false;
+    orderInformation.every((obj) => {
+      if (obj.field2 == "" && found) {
+        return false;
+      } else if (found) {
+        order.push(obj.field2);
+      } else if (obj.field2 == "Title" && !found) {
+        found = true;
+      }
+      return true;
+    });
   }
 };
-
-onChangeView();
+getOrder.open("GET", "https://docs.google.com/spreadsheets/d/1s-ItBZwDzWb_taDPD2L2jrUbNzl4pxjSgXcE5dza4tc/export?format=csv&id=1s-ItBZwDzWb_taDPD2L2jrUbNzl4pxjSgXcE5dza4tc&gid=1676553494", true);
+getOrder.send();
